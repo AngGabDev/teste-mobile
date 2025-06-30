c58912de041a59f2366691c4333c9de4f559d384
Angelo <angelogabrielpires09@gmail.com>
2025-06-30 09:05:53 -0300

    Testes mobile falhos

---
+++ wdio.conf.js
@@ -0,0 +1,38 @@
+export const config = {
+
+    runner: 'local',
+    port: 4723,
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    
+    maxInstances: 1,
+
+    capabilities: [{
+        platformName: 'Android',
+        'appium:deviceName': 'ebac',
+        'appium:platformVersion': '9.0',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': `${process.cwd}\apps\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk`,
+        'appium:appWaitActivity': '.MainActivity',
+        'appium:disableIdLocatorAutocompletion': true
+    }],
+
+    logLevel: 'info',
+
+    waitforTimeout: 10000,
+
+    connectionRetryTimeout: 120000,
+
+    connectionRetryCount: 3,
+
+    framework: 'mocha',
+
+    reporters: ['spec'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+}
